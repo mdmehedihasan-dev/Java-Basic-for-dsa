aaa73d1237f036ee1cd4d3223e3e19450885f701
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2025-09-05 16:32:00 +0600

    array

--- Array.java
+++ Array.java
@@ -1,21 +1,41 @@
 import java.util.Scanner;
 
 public class Array {
+
+    public static void upddate(int marks[]){
+        for (int i = 0; i<marks.length; i++){
+             marks[i] = marks[i] + 1;
+        }
+    }
+
+
+
+
+
+
     public static void main(String[] args) {
-        int marks[] = new int[100];
-        System.out.println("Length of array is :" + marks.length);
-        Scanner sc = new Scanner(System.in);
+        int marks[] = {97,98,99};
+        upddate(marks);
+        for (int i = 0; i<marks.length; i++){
+            System.out.println(marks[i]+" ");
+        };
+        System.out.println();
 
-        marks[0] = sc.nextInt();
-        marks[1] = sc.nextInt();
-        marks[2] = sc.nextInt();
 
-        System.out.println("phy :" + marks[0]);
-        System.out.println("che :" + marks[1]);
-        System.out.println("math :" + marks[2]);
+        // int marks[] = new int[100];
+        // System.out.println("Length of array is :" + marks.length);
+        // Scanner sc = new Scanner(System.in);
 
-        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
-        System.out.println("Avarage number is :" + percentage);
+        // marks[0] = sc.nextInt();
+        // marks[1] = sc.nextInt();
+        // marks[2] = sc.nextInt();
+
+        // System.out.println("phy :" + marks[0]);
+        // System.out.println("che :" + marks[1]);
+        // System.out.println("math :" + marks[2]);
+
+        // int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+        // System.out.println("Avarage number is :" + percentage);
 
     }
 }
