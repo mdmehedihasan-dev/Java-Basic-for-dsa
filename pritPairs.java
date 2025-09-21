7a2d0c9ea8bc501eb1d1975bd6774e9c0475d26b
Md Mehedi Hasan <116740305+mdmehedihasan-dev@users.noreply.github.com>
2025-09-21 14:45:59 +0600

    Update pritPairs.java

--- pritPairs.java
+++ pritPairs.java
@@ -1,1 +1,17 @@
 
+// "static void main" must be defined in a public class.
+public class Main {
+    public static void pritPairs(int numbers[]){
+        for(int i=0; i<numbers.length;i++){
+            int curr = numbers[i];
+            for(int j=i; j<numbers.length;j++){
+                System.out.print(""+ curr+ "," + numbers[j]);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2,3,6,5,8,4};
+        pritPairs(numbers);
+    }
+}
